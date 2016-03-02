5d6019020ab233a3975cfe4c6abd5a741fca9345
Thiyagarajan Gnanasekaran <thiyag@ThiyagaajansMBP.Home>
2016-03-02 20:36:36 +0530

    comments 2

--- src/main/java/com/fratics/precis/fis/base/Schema.java
+++ src/main/java/com/fratics/precis/fis/base/Schema.java
@@ -2,12 +2,19 @@ package com.fratics.precis.fis.base;
 
 import java.util.TreeSet;
 
+/*
+ * Schema Element of a single field in a Record of the Input Feed.
+ * 
+ */
+
 public class Schema {
 
+    //Type of the Field as Enum
     public static enum FieldType {
 	DIMENSION, METRIC, INVALID
     };
 
+    //Each Schema Element is defined by the fieldName, fieldType & fieldIndex (in the input feed).
     public class SchemaElement implements Comparable<SchemaElement> {
 	public SchemaElement(String fieldName, int fieldIndex,
 		FieldType fieldType) {
@@ -25,17 +32,21 @@ public class Schema {
 	}
     };
 
+    //List of all the SchemaElements for all the Records in the Input Feed.
     private TreeSet<SchemaElement> list = new TreeSet<SchemaElement>();
 
+    //No of Fields in the input feed.
     public int getNoOfFields() {
 	return list.size();
     }
 
+    //Add a Schema Element to the Record List.
     public void addSchemaElement(String fieldName, int fieldIndex,
 	    FieldType fieldType) {
 	addSchemaElement(new SchemaElement(fieldName, fieldIndex, fieldType));
     }
 
+    //Add a Schema Element to the Record List.
     public void addSchemaElement(SchemaElement e) {
 	list.add(e);
     }
--- src/main/java/com/fratics/precis/fis/base/ValueObject.java
+++ src/main/java/com/fratics/precis/fis/base/ValueObject.java
@@ -2,11 +2,19 @@ package com.fratics.precis.fis.base;
 
 import java.io.Serializable;
 
+/*
+ * Value Object is a Serializable Holder Object for both InputObject & OutputObject.
+ * 
+ * The Value Object is passed along to all the Flow Processors in sequence.
+ * 
+ */
+
 public class ValueObject implements Serializable {
     private static final long serialVersionUID = 1590842569304698182L;
     public InputObject inputObject = null;
     public OutputObject resultObject = null;
 
+    //A Simple toString() method, used in printing both Input & Output Objects.
     public String toString() {
 	return inputObject.toString() + "\n" + resultObject.toString();
     }
