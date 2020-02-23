b6dd9dda367f7daadb0b0692865852798a62839b
Vijay Rajan <vijayrajan@Vijay-Rajan-the-Greatest.local>
2020-02-23 16:21:24 +0530

    Compile and Running shell script

---
+++ compileAndRun.sh
@@ -0,0 +1,6 @@
+
+mvn clean package;
+time java -Xms1G -Xmx1G   -cp target/precis-0.0.1-SNAPSHOT.jar com.fratics.precis.fis.main.Main
+#java -cp target/precis-0.0.1-SNAPSHOT.jar com.fratics.precis.fis.threshold.ThresholdMain
+#time java -Xms1G -Xmx1G  -cp target/precis-0.0.1-SNAPSHOT.jar com.fratics.precis.fis.main.Main
+
